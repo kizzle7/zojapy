d950afeacafb8b0b3a508bfe47c99a2feade5f47
Victor <abimbolavictor3@gmail.com>
2024-10-13 22:52:42 +0100

    'fixes'

--- src/Views/Auth/Index.jsx
+++ src/Views/Auth/Index.jsx
@@ -1,68 +1,29 @@
 import React from 'react'
 import { AuthWizard } from "../../Components/AuthWizard/auth-wizard"
-//assets
-import Logo from "../../Assets/buddy-logo.svg";
-import Checkbox from "../../Assets/Checkbox.svg";
-//external_style
+import { AuthContent } from "../../Components/AuthWizard/AuthContents"
+import chatIcon from "../../Assets/bi_chat.svg"
 import "./index.css";
 function Index() {
     return (
         <div className="auth__bg">
             <div className="row">
-                <div className="col-md-6 col-12">
-                    <div className="auth__bg--white">
-                        <div className="auth__container">
-                            <div>
-                                <img src={Logo} />
-                            </div>
-                            <div className="auth__pane">
-                                <div className="auth-contents">
-                                    <div>
-                                        <div className="auth__contents">
-                                            <div className="pt-3">
-                                                <img src={Checkbox} />
-                                            </div>
-                                            <div className="auth__contents--text">Track real-time overview of company’s financial performance.</div>
-
-                                        </div>
-                                        <div className="auth__contents">
-                                            <div className="pt-3">
-                                                <img src={Checkbox} />
-                                            </div>
-                                            <div className="auth__contents--text">Track created projects budget against actual revenue and expenses.</div>
-
-                                        </div>
-                                        <div className="auth__contents">
-                                            <div className="pt-3">
-                                                <img src={Checkbox} />
-                                            </div>
-                                            <div className="auth__contents--text">Highlighted reports on budget deficit and surplus, accounting dimensions, balance sheets and real-time sales margin estimation.</div>
-
-                                        </div>
-                                    </div>
-
-                                </div>
-                                <br />
-                                <br />
-                                <br />
-                                <div className="text__sm--dark">
-                                    &copy;2022 Revvex. All rights reserved
-
-                                </div>
-
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
+                <AuthContent />
                 <div className="col-md-6 col-12">
                     <div className="auth__bg--smoke">
                         <div className="auth-box">
                             <div className="auth-card">
                                 <AuthWizard />
-
                             </div>
-                            <button class="chat-button" onclick="initiateChat()">Get Help</button>
+                            <br />
+                            <div className='d-flex justify-content-end align-items-center'>
+                                <button class="chat-button" >
+                                    <div className='d-flex align-items-center'>
+                                        <div className='pr-3'>Get Help</div>
+                                        <div><img src={chatIcon} /></div>
+                                    </div>
+                                </button>
+                            </div>
+
 
                         </div>
                     </div>
